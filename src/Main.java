fa714eede75d90109c7d27d2067cd0fece7b7cf9
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2024-09-02 17:31:59 -0400

    Third Commit

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,11 @@ public class Main {
     }
 
     public static double convertToCentimeters(int feet, int inches){
-        return convertToCentimeters(((feet * 12) + inches));
+        // return convertToCentimeters(feet * 12) + inches;  This is the below written in one line. The below is more readable code.
+
+        int feetToInches = feet * 12;
+        int totalInches = feetToInches + inches;
+        return convertToCentimeters(totalInches);
     }
 
 }
